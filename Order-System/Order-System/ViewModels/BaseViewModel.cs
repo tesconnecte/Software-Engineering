6a33f33d5c929b100de5b50fc5db6d732a5bb8e3
Alexis Canevali <40326915@live.napier.ac.uk>
2017-09-24 16:21:42 +0100

    Initial commit

---
+++ Order-System/Order-System/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Order_System.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ Order-System/Order-System/Views/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Order_System.Commands;
+using Order_System.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Order_System.Views
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public string AddOrderButtonContent { get; private set; }
+        public string ViewOrderButtonContent { get; private set; }
+
+        public ICommand AddOrderButtonCommand { get; private set; }
+        public ICommand ViewOrderButtonCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            AddOrderButtonContent = "Add Order";
+            ViewOrderButtonContent = "View Orders";
+
+            AddOrderButtonCommand = new RelayCommand(AddOrderButtonClick);
+            ViewOrderButtonCommand = new RelayCommand(ViewOrderButtonClick);
+
+
+        }
+
+        private void ViewOrderButtonClick()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddOrderButtonClick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
